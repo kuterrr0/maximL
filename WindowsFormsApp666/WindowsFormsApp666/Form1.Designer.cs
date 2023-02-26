dc88baf5e061f4c3ccd5374fbdd231e47a1542b6
kuterrr0 <87088307+kuterrr0@users.noreply.github.com>
2023-02-26 19:24:29 +0300

    Add files via upload

---
+++ WindowsFormsApp666/WindowsFormsApp666/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp666
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Использование ОЗУ:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(385, 365);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(385, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Получить список";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0 Кб /";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 424);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Обновить";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 453);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp666/WindowsFormsApp666/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace WindowsFormsApp666
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                updateText();
+            }
+            updateRAM();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updateText();
+        }
+
+        private void updateText()
+        {
+            richTextBox1.Clear();
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                richTextBox1.Text += String.Format("Диск {0}\nТип диска: {1}\n", d.Name, d.DriveType);
+                if (d.IsReady == true)
+                {
+                    String[] pref = { "кило", "мега", "гига", "тера" };
+
+                    long freeSpace = d.TotalFreeSpace;
+                    int divfree = 0;
+                    long totalSize = d.TotalSize;
+                    int divtotal = 0;
+
+                    while (freeSpace > 1024)
+                    {
+                        freeSpace /= 1024;
+                        divfree++;
+                    }
+                    while (totalSize > 1024)
+                    {
+                        totalSize /= 1024;
+                        divtotal++;
+                    }
+
+                    richTextBox1.Text += String.Format("\tНазвание тома: {0}\n", d.VolumeLabel);
+                    richTextBox1.Text += String.Format("\tФайловая система: {0}\n", d.DriveFormat);
+                    richTextBox1.Text += String.Format("\tДоступно: {0} байт", d.TotalFreeSpace);
+                    if (divfree > 0)
+                    {
+                        richTextBox1.Text += String.Format(" ~= {0} {1}байт\n", freeSpace, pref[divfree - 1]);
+                    }
+                    else
+                    {
+                        richTextBox1.Text += "\n";
+                    }
+
+                    richTextBox1.Text += String.Format("\tВсего: {0} байт\n", d.TotalSize);
+                    if (divtotal > 0)
+                    {
+                        richTextBox1.Text += String.Format(" ~= {0} {1}байт\n", totalSize, pref[divtotal - 1]);
+                    }
+                    else
+                    {
+                        richTextBox1.Text += "\n";
+                    }
+
+                }
+            }
+        }
+        
+        private void updateRAM()
+        {
+            Process[] processes = Process.GetProcesses();
+            long memoryUse = 0;
+
+            foreach (Process p in processes)
+            {
+                memoryUse += p.PagedMemorySize64 / (1024 * 1024);
+            }
+
+            label2.Text = memoryUse.ToString() + " Мб";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
