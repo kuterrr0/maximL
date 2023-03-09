9513cf4bbd3424669a500ebeb1ac1e4cb27fcf03
kuterrr0 <87088307+kuterrr0@users.noreply.github.com>
2023-03-10 01:20:28 +0300

    Add files via upload

---
+++ Лаба333333/Лаба333333/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Лаба333333
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Создать файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Переименовать файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Создать папку";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 324);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(137, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Перемещение файла";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 220);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(137, 46);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Удалить папку";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 272);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(137, 46);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 376);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(137, 46);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Обзор файла";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(239, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(375, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(239, 297);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(375, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox3_DragDrop);
+            this.textBox3.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox3_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Лаба333333/Лаба333333/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба333333
+{
+    public partial class Form1 : Form
+    {
+        private bool f_open, f_save;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Создать файл
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label2.Text = saveFileDialog1.FileName;
+                f_save = true;
+            }
+            else
+                label2.Text = "None";
+                f_save = false;
+        }
+
+
+        //Переименование файла
+        private void button3_Click(object sender, EventArgs e) 
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                System.IO.File.Move(textBox1.Text, textBox2.Text);
+                MessageBox.Show("Готово!");
+            }
+            
+
+            //System.IO.File.Move(textBox1.Text, textBox2.Text);               
+            //MessageBox.Show("Готово!");
+        }
+
+
+        //СОЗДАЁТ ПАПКУ (где создать, название папки)
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool exists = System.IO.Directory.Exists(textBox1.Text + "\\" + textBox2.Text);
+            if (!exists) System.IO.Directory.CreateDirectory(textBox1.Text + "\\" + textBox2.Text);
+            else MessageBox.Show("Already Exists...!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "None";
+            label2.Text = "None";
+            label3.Text = "";
+            f_open = false;
+            f_save = false;
+        }
+
+        //СОздаёт папку и переносит туда другую(то, что перенести; куда(её сущ не должно))
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bool exists = System.IO.Directory.Exists(textBox1.Text);
+            if (exists) System.IO.Directory.Move(textBox1.Text, textBox2.Text);
+        }
+
+        //Удалить папку
+        private void button6_Click(object sender, EventArgs e)
+        {
+            System.IO.Directory.Delete(textBox1.Text, true);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string source = textBox1.Text;
+            string Distination = textBox2.Text;
+            bool exists = System.IO.Directory.Exists(source);
+            if (exists)
+            {
+                System.IO.Directory.CreateDirectory(Distination);
+                System.IO.Directory.Delete(source, true);
+            }
+        }
+
+        //Обзор файла
+        private void button8_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        //Открыть файл
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+                f_open = true;
+            }
+            else
+                label1.Text = "None";
+                f_open = false;
+        }
+
+        
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void textBox3_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void textBox3_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        
+
+        
+
+    }
+}
